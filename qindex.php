b7ed39458e295998c96b5bd2875de99c9f806423
matteo-fisso <ma.addesso@gmail.com>
2019-02-20 16:22:45 +0100

    Modifica index

--- qindex.php
+++ qindex.php
@@ -8,6 +8,6 @@ $reader = new Xlsx();
 
 $workbook = $reader->load("upload/CDR-ERICSSON-MSRBS-SU_FG53RNC_Rev.6 (1).xlsx");
 $worksheet=$workbook->getSheetByName("MS-RBS");
-$row=6;
-$rows= $worksheet->getRowIterator(6);
-echo "ciao";
+
+$prova=$worksheet->getRowDimension(4);
+//echo $prova-> ;
